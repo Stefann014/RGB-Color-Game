376e28a992e0c99287ad03c60776f7d69c431c8b
Stefann014 <stefanmarjanovic1997@gmail.com>
2020-08-27 17:24:58 +0200

    RGB

--- app/src/main/java/com/example/rgbcolorgame/PocetniActivity.java
+++ app/src/main/java/com/example/rgbcolorgame/PocetniActivity.java
@@ -3,12 +3,13 @@ package com.example.rgbcolorgame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
 
-public class MainActivity extends AppCompatActivity {
+public class PocetniActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_pocetni);
     }
 }
